58c8542856ca175c5878f46a0d2b2730ef48bfd6
todays_mitsui <todays_mitsui@hatena.ne.jp>
2023-10-01 21:27:10 +0900

    feat: parse_command

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -3,6 +3,7 @@ mod identifier;
 mod lazy_k;
 mod utils;
 
+pub use ecmascript::parse_command as parse_command_with_ecmascript_style;
 pub use ecmascript::parse_expr as parse_expr_with_ecmascript_style;
 pub use lazy_k::parse_command as parse_command_with_lazy_k_style;
 pub use lazy_k::parse_expr as parse_expr_with_lazy_k_style;
