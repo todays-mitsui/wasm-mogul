89d61ad92cd3b0bfb6dbcb29577a63a1a992e65d
todays_mitsui <todays_mitsui@hatena.ne.jp>
2023-10-12 18:24:36 +0900

    パラムを読んでインプットに入れる

--- js/input.js
+++ js/input.js
@@ -2,6 +2,13 @@ export function initInput() {
   const input = document.querySelector('#input input');
   const submit = document.querySelector('#input button[type=submit]');
 
+  if (location.search) {
+    const param = new URLSearchParams(location.search);
+    if (param.has('input')) {
+      input.value = param.get('input');
+    }
+  }
+
   submit.disabled = !input.value.trim();
   input.addEventListener('input', function (event) {
     submit.disabled = !input.value.trim();
