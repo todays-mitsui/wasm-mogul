fba33e8ce88955149736c66935fa7ec82371a8d1
todays_mitsui <todays_mitsui@hatena.ne.jp>
2023-11-09 01:03:56 +0900

    いったん落ち着いた、ここからテスト

--- tuber/src/calc/evaluate3.rs
+++ tuber/src/calc/evaluate3.rs
@@ -1,6 +1,7 @@
+use super::apply::apply;
 use super::arity::arity;
 use crate::context::Context;
-use crate::expr::Expr;
+use crate::expr::{self, Expr};
 use std::{cmp, iter, slice};
 
 struct Eval {
@@ -13,47 +14,35 @@ impl Eval {
         let inventory = Inventory::new(&context, expr);
         Self { context, inventory }
     }
+}
 
-    fn next_path(&self) -> Option<Vec<usize>> {
-        let mut path = Vec::new();
-        let mut inventory = &self.inventory;
-        loop {
-            match inventory.get_reducible() {
-                ReducibleResult::Callee { .. } => {
-                    return Some(path);
-                }
-                ReducibleResult::Arg { index, arg } => {
-                    path.push(index);
-                    inventory = arg;
-                }
-                ReducibleResult::None => {
-                    return None;
-                }
-            }
-        }
-    }
+impl Iterator for Eval {
+    type Item = Expr;
 
-    fn get_next(&self) -> Option<&Inventory> {
-        let mut inventory = &self.inventory;
-        loop {
-            match inventory.get_reducible() {
-                ReducibleResult::Callee { .. } => {
-                    return Some(inventory);
-                }
-                ReducibleResult::Arg { index: _index, arg } => {
-                    inventory = arg;
-                }
-                ReducibleResult::None => {
-                    return None;
-                }
-            }
+    fn next(&mut self) -> Option<Self::Item> {
+        let inventory = self.inventory.get_next()?;
+
+        let arity = inventory.arity?;
+        let args = inventory.args.shift(arity)?;
+        let mut callee = &mut inventory.callee;
+
+        apply(&self.context, callee, args).ok()?;
+        let expr = callee.clone();
+
+        while let Expr::Apply { lhs, rhs } = callee {
+            callee = lhs;
+            inventory.args.unshift(&self.context, *rhs.to_owned());
         }
+
+        inventory.callee = callee.to_owned();
+
+        Some(expr)
     }
 }
 
 // ========================================================================== //
 
-#[derive(Debug, PartialEq)]
+#[derive(Clone, Debug, PartialEq)]
 struct Inventory {
     callee: Expr,
     arity: Option<usize>,
@@ -76,10 +65,7 @@ impl Inventory {
         Self {
             callee,
             arity,
-            redex: args
-                .iter::<&Inventory>()
-                .map(|arg| arg.reducible())
-                .collect(),
+            redex: args.iter().map(|arg| arg.reducible()).collect(),
             args,
         }
     }
@@ -88,15 +74,15 @@ impl Inventory {
         self.arity.is_some() || self.redex.iter().any(|b| *b)
     }
 
-    fn get_reducible(&self) -> ReducibleResult {
+    fn get_reducible(&mut self) -> ReducibleResult {
         match self.arity {
             Some(arity) => ReducibleResult::Callee {
-                expr: &self.callee,
+                expr: &mut self.callee,
                 arity,
             },
             None => match self
                 .args
-                .enumerate::<(usize, &Inventory)>()
+                .enumerate_mut::<(usize, &mut Inventory)>()
                 .find(|(_index, arg)| arg.reducible())
             {
                 Some((index, arg)) => ReducibleResult::Arg { index, arg },
@@ -104,11 +90,69 @@ impl Inventory {
             },
         }
     }
+
+    fn next_path(&self) -> Option<Vec<usize>> {
+        let mut path = Vec::new();
+        let mut inventory = self;
+        loop {
+            if let Some(_) = inventory.arity {
+                return Some(path);
+            } else {
+                match inventory
+                    .args
+                    .enumerate::<(usize, &Inventory)>()
+                    .find(|(_index, arg)| arg.reducible())
+                {
+                    Some((index, arg)) => {
+                        path.push(index);
+                        inventory = arg;
+                    }
+                    None => return None,
+                }
+            }
+        }
+    }
+
+    fn get_next(&mut self) -> Option<&mut Inventory> {
+        let mut inventory = self;
+        loop {
+            if let Some(_) = inventory.arity {
+                return Some(inventory);
+            } else {
+                match inventory
+                    .args
+                    .enumerate_mut::<(usize, &mut Inventory)>()
+                    .find(|(_index, arg)| arg.reducible())
+                {
+                    Some((_index, arg)) => {
+                        inventory = arg;
+                    }
+                    None => return None,
+                }
+            }
+        }
+    }
+}
+
+impl From<Inventory> for Expr {
+    fn from(inventory: Inventory) -> Self {
+        let mut expr = inventory.callee;
+        for arg in inventory.args.0.into_iter() {
+            expr = expr::a(expr, Expr::from(arg));
+        }
+        expr
+    }
 }
 
 enum ReducibleResult<'a> {
-    Callee { expr: &'a Expr, arity: usize },
-    Arg { index: usize, arg: &'a Inventory },
+    Callee {
+        expr: &'a mut Expr,
+        arity: usize,
+    },
+    Arg {
+        index: usize,
+        arg: &'a mut Inventory,
+    },
     None,
 }
 
@@ -117,7 +161,7 @@ enum ReducibleResult<'a> {
 /// ラムダ式の部分式のうち引数部分を保持する両端キュー
 /// 実装の都合で内部的には引数を逆順で保持する
 /// ```sxyz を分解して格納した場合、外部的には [x, y, z] として振る舞い、内部的には [z, y, x] というデータを保持する
-#[derive(Debug, PartialEq)]
+#[derive(Clone, Debug, PartialEq)]
 struct Args(Vec<Inventory>);
 
 impl Args {
@@ -129,20 +173,74 @@ impl Args {
         self.0.len()
     }
 
+    fn shift(&mut self, n: usize) -> Option<Vec<Expr>> {
+        let length = self.len();
+
+        if length >= n {
+            Some(
+                self.0
+                    .drain(length - n..)
+                    .rev()
+                    .map(|inventory| inventory.into())
+                    .collect(),
+            )
+        } else {
+            None
+        }
+    }
+
     fn unshift(&mut self, context: &Context, expr: Expr) {
         let inventory = Inventory::new(context, expr);
         self.0.push(inventory)
+    }
+
+    fn join(&mut self, other: Self) {
+        self.0.extend(other.0.into_iter().rev())
+    }
+
+    fn iter(&self) -> ArgsIter {
+        ArgsIter::new(self.to_owned())
     }
 
     fn enumerate<Iter>(&self) -> iter::Enumerate<iter::Rev<slice::Iter<'_, Inventory>>> {
         self.0.iter().rev().enumerate()
     }
 
-    // fn iter(&self) -> impl Iterator<Item = (usize, &Inventory)> {
-    // TODO: このように impl Trait の形で書くとうまくいかない
-    // TODO: Error: cannot move out of `args` because it is borrowed
-    fn iter<Iter>(&self) -> iter::Rev<slice::Iter<'_, Inventory>> {
-        self.0.iter().rev()
+    fn enumerate_mut<Iter>(&mut self) -> iter::Enumerate<iter::Rev<slice::IterMut<'_, Inventory>>> {
+        self.0.iter_mut().rev().enumerate()
+    }
+
+    // // fn iter(&self) -> impl Iterator<Item = (usize, &Inventory)> {
+    // // TODO: このように impl Trait の形で書くとうまくいかない
+    // // TODO: Error: cannot move out of `args` because it is borrowed
+    // fn iter<Iter>(&self) -> iter::Rev<slice::Iter<'_, Inventory>> {
+    //     self.0.iter().rev()
+    // }
+}
+
+impl From<Vec<Inventory>> for Args {
+    fn from(args: Vec<Inventory>) -> Self {
+        Self(args)
+    }
+}
+
+struct ArgsIter {
+    iter: std::iter::Rev<std::vec::IntoIter<Inventory>>,
+}
+
+impl ArgsIter {
+    fn new(args: Args) -> Self {
+        Self {
+            iter: args.0.into_iter().rev(),
+        }
+    }
+}
+
+impl Iterator for ArgsIter {
+    type Item = Inventory;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.iter.next()
     }
 }
 
@@ -191,43 +289,43 @@ mod tests {
     }
 
     #[test]
-    fn test_eval_next_path() {
+    fn test_inventory_next_path() {
         let context = setup();
 
         let expr = expr::s("TRUE");
-        let eval = Eval::new(context.clone(), expr);
-        assert_eq!(eval.next_path(), None);
+        let inventory = Inventory::new(&context, expr);
+        assert_eq!(inventory.next_path(), None);
 
         let expr = expr::v("TRUE");
-        let eval = Eval::new(context.clone(), expr);
-        assert_eq!(eval.next_path(), None);
+        let inventory = Inventory::new(&context, expr);
+        assert_eq!(inventory.next_path(), None);
 
         let expr = expr::a(":i", ":x");
-        let eval = Eval::new(context.clone(), expr);
-        assert_eq!(eval.next_path(), None);
+        let inventory = Inventory::new(&context, expr);
+        assert_eq!(inventory.next_path(), None);
 
         let expr = expr::a("i", ":x");
-        let eval = Eval::new(context.clone(), expr);
-        assert_eq!(eval.next_path(), Some(vec![]));
+        let inventory = Inventory::new(&context, expr);
+        assert_eq!(inventory.next_path(), Some(vec![]));
 
         let expr = expr::a(expr::a("i", ":x"), ":y");
-        let eval = Eval::new(context.clone(), expr);
-        assert_eq!(eval.next_path(), Some(vec![]));
+        let inventory = Inventory::new(&context, expr);
+        assert_eq!(inventory.next_path(), Some(vec![]));
 
         let expr = expr::a(":f", expr::a("i", ":x"));
-        let eval = Eval::new(context.clone(), expr);
-        assert_eq!(eval.next_path(), Some(vec![0]));
+        let inventory = Inventory::new(&context, expr);
+        assert_eq!(inventory.next_path(), Some(vec![0]));
 
         let expr = expr::a(expr::a("i", ":x"), expr::a("i", ":y"));
-        let eval = Eval::new(context.clone(), expr);
-        assert_eq!(eval.next_path(), Some(vec![]));
+        let inventory = Inventory::new(&context, expr);
+        assert_eq!(inventory.next_path(), Some(vec![]));
 
         let expr = expr::a(expr::a(":i", ":x"), expr::a("i", ":y"));
-        let eval = Eval::new(context.clone(), expr);
-        assert_eq!(eval.next_path(), Some(vec![1]));
+        let inventory = Inventory::new(&context, expr);
+        assert_eq!(inventory.next_path(), Some(vec![1]));
 
         let expr = expr::a(":g", expr::a(":f", expr::a("i", ":y")));
-        let eval = Eval::new(context.clone(), expr);
-        assert_eq!(eval.next_path(), Some(vec![0, 0]));
+        let inventory = Inventory::new(&context, expr);
+        assert_eq!(inventory.next_path(), Some(vec![0, 0]));
     }
 }
