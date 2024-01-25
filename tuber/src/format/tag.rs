1bb06f169265a9858b38e36dba86030fba8a1330
todays_mitsui <todays_mitsui@hatena.ne.jp>
2024-01-25 22:04:10 +0900

    range を取れるようにした

--- tuber/src/format/tag.rs
+++ tuber/src/format/tag.rs
@@ -1,3 +1,6 @@
+use crate::expr::Path;
+use std::ops::Range;
+
 #[derive(Clone, PartialEq)]
 pub struct Tag(Vec<usize>);
 
@@ -41,5 +44,116 @@ impl std::fmt::Debug for Tag {
 impl From<Vec<usize>> for Tag {
     fn from(indices: Vec<usize>) -> Self {
         Self(indices)
+    }
+}
+
+impl Path {
+    pub fn range(&self, tags: &[Tag]) -> Option<Range<usize>> {
+        let (prefix, arity) = self.prefix_and_arity();
+        let len = prefix.len();
+
+        let predicate = |tag: &Tag| {
+            // println!("   tag: {:?}", tag.0);
+            // println!("prefix: {:?}, {}", prefix, arity);
+            prefix
+                .iter()
+                .enumerate()
+                .all(|(i, v)| tag.0.get(i) == Some(v))
+                && tag.0.get(len).map(|index| *index <= arity).unwrap_or(false)
+        };
+
+        let mut tags_iter = tags.iter();
+
+        let start = tags_iter.position(predicate)?;
+        let end = tags_iter
+            .position(|tag| !predicate(tag))
+            .unwrap_or(tags.len() - 1);
+
+        Some(Range {
+            start,
+            end: start + 1 + end,
+        })
+    }
+
+    fn prefix_and_arity(&self) -> (Vec<usize>, usize) {
+        let mut path = self;
+
+        let mut prefix: Vec<usize> = Vec::new();
+        while let Path::Arg(index, next) = path {
+            prefix.push(*index);
+            path = &**next;
+        }
+
+        let arity: usize = if let Path::Callee(arity) = path {
+            *arity
+        } else {
+            panic!("Path::range() called on non-callee path");
+        };
+
+        (prefix, arity)
+    }
+}
+
+// ========================================================================== //
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::expr;
+
+    #[test]
+    fn test_range() {
+        let tags = vec![
+            /*  0: a */ Tag::from(vec![0]),
+            /*  1: ( */ Tag::from(vec![4]),
+            /*  2: b */ Tag::from(vec![1, 0]),
+            /*  3: , */ Tag::from(vec![4]),
+            /*  4:   */ Tag::from(vec![4]),
+            /*  5: c */ Tag::from(vec![2, 0]),
+            /*  6: , */ Tag::from(vec![4]),
+            /*  7:   */ Tag::from(vec![4]),
+            /*  8: d */ Tag::from(vec![3, 0]),
+            /*  9: ( */ Tag::from(vec![3, 2]),
+            /* 10: e */ Tag::from(vec![3, 1, 0]),
+            /* 11: , */ Tag::from(vec![3, 2]),
+            /* 12:   */ Tag::from(vec![3, 2]),
+            /* 13: f */ Tag::from(vec![3, 2, 0]),
+            /* 14: ) */ Tag::from(vec![3, 2]),
+            /* 15: , */ Tag::from(vec![4]),
+            /* 16:   */ Tag::from(vec![4]),
+            /* 17: g */ Tag::from(vec![4, 0]),
+            /* 18: ( */ Tag::from(vec![4, 1]),
+            /* 19: h */ Tag::from(vec![4, 1, 0]),
+            /* 20: ) */ Tag::from(vec![4, 1]),
+            /* 21: ) */ Tag::from(vec![4]),
+        ];
+
+        let path = Path::Callee(0);
+        let range = path.range(&tags).unwrap();
+        assert_eq!(range, 0..1);
+
+        let path = Path::Arg(1, Box::new(Path::Callee(0)));
+        let range = path.range(&tags).unwrap();
+        assert_eq!(range, 2..3);
+
+        let path = Path::Arg(3, Box::new(Path::Arg(1, Box::new(Path::Callee(0)))));
+        let range = path.range(&tags).unwrap();
+        assert_eq!(range, 10..11);
+
+        let path = Path::Arg(3, Box::new(Path::Callee(2)));
+        let range = path.range(&tags).unwrap();
+        assert_eq!(range, 8..15);
+
+        let path = Path::Arg(4, Box::new(Path::Callee(0)));
+        let range = path.range(&tags).unwrap();
+        assert_eq!(range, 17..18);
+
+        let path = Path::Arg(4, Box::new(Path::Callee(1)));
+        let range = path.range(&tags).unwrap();
+        assert_eq!(range, 17..21);
+
+        let path = Path::Callee(4);
+        let range = path.range(&tags).unwrap();
+        assert_eq!(range, 0..22);
     }
 }
