d88d1ab79f6338608d69f19ba36b365d1472cf02
todays_mitsui <todays_mitsui@hatena.ne.jp>
2024-02-17 22:57:50 +0900

    addEventListener

--- js/settings.js
+++ js/settings.js
@@ -15,9 +15,21 @@ export function initSettings(module) {
       updateContext(module);
     });
   }
+
+  const resetButton = document.querySelector(`button[name=${KEY_RESET_CONTEXT}]`);
+  if (resetButton) {
+    resetButton.addEventListener('click', resetContext);
+  }
+
+  const clearButton = document.querySelector(`button[name=${KEY_CLEAR_CONTEXT}]`);
+  if (clearButton) {
+    clearButton.addEventListener('click', clearContext);
+  }
 }
 
 const KEY_DISPLAY_STYLE = 'tuber_display_style';
+const KEY_RESET_CONTEXT = 'tuber_reset_context';
+const KEY_CLEAR_CONTEXT = 'tuber_clear_context';
 
 export function getDisplayStyle() {
   const strage = window.localStorage;
@@ -27,4 +39,16 @@ export function getDisplayStyle() {
 function setDisplayStyle(value) {
   const strage = window.localStorage;
   strage.setItem(KEY_DISPLAY_STYLE, value);
+}
+
+function resetContext() {
+  console.log('resetContext');
+  if (confirm('Context が初期状態に戻されます。よろしいですか？')) {
+  }
+}
+
+function clearContext() {
+  console.log('clearContext');
+  if (confirm('Context に登録された全ての Function が削除されます。よろしいですか？')) {
+  }
 }
