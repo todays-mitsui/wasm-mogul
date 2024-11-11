48e104cc5f8457c1434ac58738b5e3a0375b0786
todays_mitsui <todays_mitsui@hatena.ne.jp>
2024-11-12 08:07:47 +0900

    ちゃんと型が付くようにContextを書き換える

--- ski3/src/context.rs
+++ ski3/src/context.rs
@@ -1,34 +1,32 @@
 use crate::function::Func;
 use crate::identifier::Identifier;
-use serde_wasm_bindgen;
+use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
-use tsify_next::declare;
+use tsify_next::Tsify;
 use tuber;
 use wasm_bindgen::prelude::*;
 
-#[declare]
-pub type Context = HashMap<Identifier, Func>;
+#[derive(Tsify, Serialize, Deserialize)]
+#[tsify(into_wasm_abi, from_wasm_abi)]
+pub struct Context(HashMap<Identifier, Func>);
 
-pub fn to_tuber_context(ski_context: Context) -> tuber::Context {
-    ski_context
-        .into_iter()
-        .map(|(id, func)| (id.into(), func.into()))
-        .collect::<HashMap<tuber::Identifier, tuber::Func>>()
-        .into()
+impl From<tuber::Context> for Context {
+    fn from(tuber_context: tuber::Context) -> Context {
+        Context(
+            tuber_context
+                .into_iter()
+                .map(|(id, func)| (id.as_str().into(), func.into()))
+                .collect::<HashMap<Identifier, Func>>(),
+        )
+    }
 }
 
-pub fn from_tuber_context(tuber_context: tuber::Context) -> Context {
-    tuber_context
-        .into_iter()
-        .map(|(id, func)| (id.as_str().into(), func.into()))
-        .collect::<HashMap<Identifier, Func>>()
-}
-
-#[wasm_bindgen(typescript_custom_section)]
-const TS_PRINT_CONTEXT: &'static str = "export function printContext(context: Context): string;";
-
-#[wasm_bindgen(js_name = printContext)]
-pub fn print_context(context: JsValue) -> String {
-    let context = serde_wasm_bindgen::from_value::<Context>(context);
-    format!("{:?}", context)
+impl From<Context> for tuber::Context {
+    fn from(ski_context: Context) -> tuber::Context {
+        ski_context
+            .into_iter()
+            .map(|(id, func)| (id.into(), func.into()))
+            .collect::<HashMap<tuber::Identifier, tuber::Func>>()
+            .into()
+    }
 }
