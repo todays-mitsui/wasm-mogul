1aa1ec9e32d425f11fd9bfc9f7426f840dc60973
todays_mitsui <todays_mitsui@hatena.ne.jp>
2024-11-22 14:08:53 +0900

    hcm導入

--- mogul/src/components/ParseExpr.tsx
+++ mogul/src/components/ParseExpr.tsx
@@ -1,21 +1,25 @@
 import { createSignal } from "solid-js";
-import { parseExpr } from '../../../ski3/pkg/index';
+import { type Expr, parseExpr } from '../../../ski3/pkg/index';
+import styles from './ParseExpr.module.css';
 
 export default function ParseExpr() {
   const [inputStr, setInputStr] = createSignal('');
-  const expr = () => {
+  const [parsedExpr, setParsedExpr] = createSignal<Expr | null>(null);
+  const onClick = () => {
     try {
-      return parseExpr(inputStr());
+      const expr = parseExpr(inputStr());
+      setParsedExpr(expr);
     } catch (err) {
       console.error(err);
     }
   };
 
   return (
-    <div>
+    <div class={styles.parse_expr}>
       <input value={inputStr()} onInput={(event) => setInputStr(event.target.value)} />
+      <button onClick={onClick}>Parse</button>
       <div>
-        <pre><code>{JSON.stringify(expr(), null, 2)}</code></pre>
+        <pre><code>{JSON.stringify(parsedExpr(), null, 2)}</code></pre>
       </div>
     </div>
   );
