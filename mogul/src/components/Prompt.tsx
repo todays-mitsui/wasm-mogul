76249f211ec79082b7e50f1bc5b25c0ba910007d
todays_mitsui <todays_mitsui@hatena.ne.jp>
2024-12-11 19:17:32 +0900

    履歴表示

--- mogul/src/components/Prompt.tsx
+++ mogul/src/components/Prompt.tsx
@@ -57,16 +57,112 @@ export default function Prompt(props: Props): JSX.Element {
             Run
           </button>
         </div>
-        <ElasticTextarea
+        <HistoryInput
           class={styles.input}
           value={commandStr()}
           placeholder={focus() ? "" : "_"}
           onFocusIn={() => setFocus(true)}
           onFocusOut={() => setFocus(false)}
           onInput={(event) => setCommandStr(event.target.value)}
+          history={commandHistory()}
         />
       </form>
     </div>
+  );
+}
+
+// ========================================================================== //
+
+type TextareaKeyDownEvent = KeyboardEvent & {
+  currentTarget: HTMLTextAreaElement;
+  target: HTMLTextAreaElement;
+};
+
+interface HistoryInputProps extends ElasticInputProps {
+  history: string[];
+  value: string;
+  onInput?: JSX.EventHandler<HTMLTextAreaElement, TextareaInputEvent>;
+  onKeyDown?: JSX.EventHandler<HTMLTextAreaElement, TextareaKeyDownEvent>;
+}
+
+export function HistoryInput(props: HistoryInputProps): JSX.Element {
+  const [, remainingProps] = splitProps(props, [
+    "value",
+    "history",
+    "onInput",
+    "onKeyDown",
+  ]);
+
+  const [text, setText] = createSignal("");
+  const [historyIndex, setHistoryIndex] = createSignal(-1);
+  const [typingText, setTypingText] = createSignal(""); // 入力中未確定テキストを逃がしておくための signal
+
+  createEffect(() => {
+    setText(props.value);
+  });
+
+  const onInput: JSX.EventHandler<HTMLTextAreaElement, TextareaInputEvent> = (
+    event,
+  ) => {
+    setText(event.target.value);
+    props.onInput?.(event);
+  };
+
+  const onKeyDown: JSX.EventHandler<
+    HTMLTextAreaElement,
+    TextareaKeyDownEvent
+  > = (event) => {
+    if (event.key === "ArrowUp") {
+      // カーソル位置と1行目の判定
+      const textarea = event.target;
+      const cursorPosition = textarea.selectionStart;
+      const currentLine = textarea.value
+        .substring(0, cursorPosition)
+        .split("\n").length;
+
+      if (currentLine === 1) {
+        if (historyIndex() < 0) {
+          setTypingText(text());
+        }
+        if (historyIndex() < props.history.length - 1) {
+          const newIndex = historyIndex() + 1;
+          setHistoryIndex(newIndex);
+          setText(props.history[props.history.length - 1 - newIndex]);
+          event.preventDefault(); // デフォルト動作を防止
+        }
+      }
+    }
+
+    if (event.key === "ArrowDown") {
+      const textarea = event.target;
+      const cursorPosition = textarea.selectionStart;
+      const currentLine = textarea.value
+        .substring(0, cursorPosition)
+        .split("\n").length;
+      const lastLine = textarea.value.split("\n").length;
+
+      if (currentLine === lastLine && historyIndex() > 0) {
+        const newIndex = historyIndex() - 1;
+        setHistoryIndex(newIndex);
+        setText(props.history[props.history.length - 1 - newIndex]);
+        event.preventDefault();
+      } else if (currentLine === lastLine && historyIndex() === 0) {
+        setHistoryIndex(-1);
+        setText(typingText());
+        event.preventDefault();
+      }
+    }
+
+    props.onKeyDown?.(event);
+  };
+
+  return (
+    <ElasticInput
+      value={text()}
+      onInput={onInput}
+      onKeyDown={onKeyDown}
+      {...remainingProps}
+    />
   );
 }
 
@@ -77,38 +173,31 @@ type TextareaInputEvent = InputEvent & {
   target: HTMLTextAreaElement;
 };
 
-interface ElasticTextareaProps {
+interface ElasticInputProps {
+  value: string;
   onInput?: JSX.EventHandler<HTMLTextAreaElement, TextareaInputEvent>;
-  ref?: HTMLTextAreaElement;
-  style?: Record<string, string>;
+  rows?: number;
   [key: string]: any;
 }
 
-function ElasticTextarea(props: ElasticTextareaProps): JSX.Element {
-  const [, remainingProps] = splitProps(props, ["onInput", "ref", "style"]);
+function ElasticInput(props: ElasticInputProps): JSX.Element {
+  const [, remainingProps] = splitProps(props, ["value", "onInput", "rows"]);
 
-  const [height, setHeight] = createSignal(0);
   const onInput: JSX.EventHandler<HTMLTextAreaElement, TextareaInputEvent> = (
     event,
   ) => {
-    setHeight(event.currentTarget.scrollHeight);
+    const target = event.target;
+    target.style.height = "auto";
+    target.style.height = `${target.scrollHeight}px`;
+
     props.onInput?.(event);
   };
 
-  let textareaRef: HTMLTextAreaElement | undefined;
-  createEffect(() => {
-    if (textareaRef != null) {
-      props.ref = textareaRef;
-      setHeight(textareaRef.scrollHeight);
-    }
-  });
-
   return (
     <textarea
-      value={commandStr()}
+      value={props.value}
       onInput={onInput}
-      ref={textareaRef}
-      style={{ ...props.style, height: `${height()}px` }}
+      rows={props.rows ?? 1}
       {...remainingProps}
     />
   );
