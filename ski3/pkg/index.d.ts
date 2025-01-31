9f331a0e93c2fa9e9f1be5421734d035a1d40f31
todays_mitsui <todays_mitsui@hatena.ne.jp>
2025-01-31 20:30:31 +0900

    ski:build

--- ski3/pkg/index.d.ts
+++ ski3/pkg/index.d.ts
@@ -3,11 +3,11 @@
 export function parseCommand(input: string): Command;
 export function parseExpr(input: string): Expr;
 export function renderExpr(expr: Expr, displayStyle: DisplayStyle): string;
+export function renderFunc(func: Func, displayStyle: DisplayStyle): string;
 export function expand(context: Context, expr: Expr): Expr;
 export function unlambdaRecursive(context: Context, expr: Expr): Expr;
 export function unlambdaRecursive_(context: Context, expr: Expr): Expr;
 export function unlambdaIota(context: Context, expr: Expr): Expr;
-export function renderFunc(func: Func, displayStyle: DisplayStyle): string;
 export function defaultContext(): Context;
 export type Command = { type: "Delete"; identifier: string } | { type: "Update"; func: Func } | { type: "Reduce"; expr: Expr } | { type: "ReduceLast"; expr: Expr } | { type: "ReduceHead"; count: number; expr: Expr } | { type: "ReduceTail"; count: number; expr: Expr } | { type: "Query"; identifier: string } | { type: "Context" } | { type: "Unlambda"; level: number; expr: Expr };
 
@@ -41,17 +41,17 @@ export interface ReducibleRange {
     args: ExprRange[];
 }
 
-export type Expr = { Variable: { identifier: string } } | { Symbol: { identifier: string } } | { Apply: { lhs: Expr; rhs: Expr } } | { Lambda: { param: string; body: Expr } };
+export type Expr = { V: { i: string } } | { S: { i: string } } | { A: { l: Expr; r: Expr } } | { L: { p: string; b: Expr } };
+
+export interface Func {
+    n: Identifier;
+    p: Identifier[];
+    b: Expr;
+}
 
 export type Aliases = Record<Identifier, Expr>;
 
 export type DisplayStyle = "EcmaScript" | "LazyK";
-
-export interface Func {
-    name: Identifier;
-    params: Identifier[];
-    body: Expr;
-}
 
 export type Context = Record<Identifier, Func>;
 
--- ski3/pkg/index_bg.js
+++ ski3/pkg/index_bg.js
@@ -162,6 +162,24 @@ export function renderExpr(expr, displayStyle) {
 }
 
 /**
+ * @param {Func} func
+ * @param {DisplayStyle} displayStyle
+ * @returns {string}
+ */
+export function renderFunc(func, displayStyle) {
+    let deferred1_0;
+    let deferred1_1;
+    try {
+        const ret = wasm.renderFunc(func, displayStyle);
+        deferred1_0 = ret[0];
+        deferred1_1 = ret[1];
+        return getStringFromWasm0(ret[0], ret[1]);
+    } finally {
+        wasm.__wbindgen_free(deferred1_0, deferred1_1, 1);
+    }
+}
+
+/**
  * @param {Context} context
  * @param {Expr} expr
  * @returns {Expr}
@@ -199,24 +217,6 @@ export function unlambdaRecursive_(context, expr) {
 export function unlambdaIota(context, expr) {
     const ret = wasm.unlambdaIota(context, expr);
     return ret;
-}
-
-/**
- * @param {Func} func
- * @param {DisplayStyle} displayStyle
- * @returns {string}
- */
-export function renderFunc(func, displayStyle) {
-    let deferred1_0;
-    let deferred1_1;
-    try {
-        const ret = wasm.renderFunc(func, displayStyle);
-        deferred1_0 = ret[0];
-        deferred1_1 = ret[1];
-        return getStringFromWasm0(ret[0], ret[1]);
-    } finally {
-        wasm.__wbindgen_free(deferred1_0, deferred1_1, 1);
-    }
 }
 
 /**
