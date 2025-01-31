fcf406f44c8b1484ae16bb7cea7a4eb2d4e18d02
todays_mitsui <todays_mitsui@hatena.ne.jp>
2025-01-31 13:33:39 +0900

    ski:build

--- ski3/pkg/index.d.ts
+++ ski3/pkg/index.d.ts
@@ -3,11 +3,11 @@
 export function parseCommand(input: string): Command;
 export function parseExpr(input: string): Expr;
 export function renderExpr(expr: Expr, displayStyle: DisplayStyle): string;
-export function renderFunc(func: Func, displayStyle: DisplayStyle): string;
 export function expand(context: Context, expr: Expr): Expr;
 export function unlambdaRecursive(context: Context, expr: Expr): Expr;
 export function unlambdaRecursive_(context: Context, expr: Expr): Expr;
 export function unlambdaIota(context: Context, expr: Expr): Expr;
+export function renderFunc(func: Func, displayStyle: DisplayStyle): string;
 export function defaultContext(): Context;
 export type Command = { type: "Delete"; identifier: string } | { type: "Update"; func: Func } | { type: "Reduce"; expr: Expr } | { type: "ReduceLast"; expr: Expr } | { type: "ReduceHead"; count: number; expr: Expr } | { type: "ReduceTail"; count: number; expr: Expr } | { type: "Query"; identifier: string } | { type: "Context" } | { type: "Unlambda"; level: number; expr: Expr };
 
@@ -43,13 +43,15 @@ export interface ReducibleRange {
 
 export type Expr = { Variable: { identifier: string } } | { Symbol: { identifier: string } } | { Apply: { lhs: Expr; rhs: Expr } } | { Lambda: { param: string; body: Expr } };
 
+export type Aliases = Record<Identifier, Expr>;
+
+export type DisplayStyle = "EcmaScript" | "LazyK";
+
 export interface Func {
     name: Identifier;
     params: Identifier[];
     body: Expr;
 }
-
-export type DisplayStyle = "EcmaScript" | "LazyK";
 
 export type Context = Record<Identifier, Func>;
 
@@ -57,7 +59,7 @@ export type Identifier = string;
 
 export class Reducer {
   free(): void;
-  constructor(context: Context, expr: Expr, displayStyle?: DisplayStyle | null);
+  constructor(context: Context, aliases: Aliases, expr: Expr, displayStyle?: DisplayStyle | null);
   next(): IteratorResult;
   displayStyle: DisplayStyle;
   readonly formed: FormedExpr;
